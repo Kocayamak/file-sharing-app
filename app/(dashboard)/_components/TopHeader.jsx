8412605ebdfcfd833e03384c188c8eb4fa39dcaa
Kocayamak <mikailkocayamak@gmail.com>
2023-12-08 17:46:11 +0300

    dashboard topheader ui completed

---
+++ app/(dashboard)/_components/TopHeader.jsx
@@ -0,0 +1,17 @@
+import { UserButton } from '@clerk/nextjs'
+import { AlignJustify } from 'lucide-react'
+import Image from 'next/image'
+import React from 'react'
+
+
+const TopHeader = () => {
+  return (
+    <div className='flex p-5 border-b items-center justify-between md:justify-end'>
+        <AlignJustify  className='md:hidden' />
+        <Image className='md:hidden' src='/logo.svg' width={150} height={100} />
+        <UserButton />
+    </div>
+  )
+}
+
+export default TopHeader
